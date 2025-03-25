491f2d3fc4c57b1a11daae3b57be3048b9a209d7
rushil <rushiljalal@gmail.com>
2025-03-25 10:08:43 +0530

    fixed active page in header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function Header() {
+  const location = useLocation();
+
   return (
     <Navbar bg="dark" variant="dark" className="shadow">
       <Container
@@ -24,13 +26,34 @@ function Header() {
           Video Calling
         </Navbar.Brand>
         <Nav style={{ display: "flex", gap: "15px" }}>
-          <Nav.Link as={Link} to="/" style={{ fontWeight: "500" }}>
+          <Nav.Link
+            as={Link}
+            to="/"
+            style={{
+              fontWeight: "500",
+              color: location.pathname === "/" ? "#fff" : "#ccc",
+            }}
+          >
             Home
           </Nav.Link>
-          <Nav.Link as={Link} to="/history" style={{ fontWeight: "500" }}>
+          <Nav.Link
+            as={Link}
+            to="/history"
+            style={{
+              fontWeight: "500",
+              color: location.pathname === "/history" ? "#fff" : "#ccc",
+            }}
+          >
             History
           </Nav.Link>
-          <Nav.Link as={Link} to="/settings" style={{ fontWeight: "500" }}>
+          <Nav.Link
+            as={Link}
+            to="/settings"
+            style={{
+              fontWeight: "500",
+              color: location.pathname === "/settings" ? "#fff" : "#ccc",
+            }}
+          >
             Settings
           </Nav.Link>
         </Nav>
